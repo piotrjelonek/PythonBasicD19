c7921571354841256b456c61c65b2f419e2e56c9
MikiKru <michal_kruczkowski@o2.pl>
2019-11-27 09:56:57 +0100

    python start

---
+++ FirstCommands.py
@@ -0,0 +1,12 @@
+# CTRL + / -> komantarz jednowierszowy
+'''
+komentarz
+wielo-
+linijkowy
+'''
+print('Witaj w Pythonie')
+# zmienne -> zeklaracja i przypisanie -> inicjalizacja zmiennej (obiektu)
+text = "Witaj"
+name = "Michał"
+sign = 'a'
+print(text + " " + name)
