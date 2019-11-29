85c07f138895453c13ce19e7eb42479d9d43a5e0
MikiKru <michal_kruczkowski@o2.pl>
2019-11-29 10:13:48 +0100

    repetition

--- Loops.py
+++ Loops.py
@@ -10,17 +10,41 @@ while(True):
 
 
 # Wprowadź dane z klawiatury do listy tak długo aż użytkownik wpisze Q
-elements = []
-flag = True
-while(flag):
-    elem  = input("podaj wartość lub wprowadź Q zeby wyjść")
-    if(elem.upper() == "Q"):
-        print("wyjście")
-        flag = not flag
-        continue
-    elements.append(elem)
-else:
-    print(elements)
+# elements = []
+# flag = True
+# while(flag):
+#     elem  = input("podaj wartość lub wprowadź Q zeby wyjść")
+#     if(elem.upper() == "Q"):
+#         print("wyjście")
+#         flag = not flag
+#         continue
+#     elements.append(elem)
+# else:
+#     print(elements)
+
+# Dana jest lista wartości
+a = [0.3, 0.4, 3.2, 0.31, 1, 5.4, 2, 1, 17, 7]
+# Napisz program filtrujący wartości po zadanym przez użytkownika progu
+# tj. w tablicy wynikowej pojawiają się tylko wartości większe od treshold
+tresholdLow = float(input("podaj próg odcięcia dolny"))
+tresholdTop = float(input("podaj próg odcięcia górny"))
+categorizedResult = []
+class0 = []     # lista przechowująca wartości skalsyfikowane jako 0
+class1 = []     # lista przechowująca wartości skalsyfikowane jako 1
+labelsDict = {0 : class0, 1 : class1}
+index = 0
+while(index < len(a)):
+    if(a[index] > tresholdLow and a[index] < tresholdTop):
+        categorizedResult.append(1)
+        class1.append(a[index])
+    else:
+        categorizedResult.append(0)
+        class0.append(a[index])
+    index += 1
+
+print(a)
+print(categorizedResult)
+print(labelsDict)
 
 
 
