317d0c2c75124a5612b9bf4ee41d6cfddd1b69a1
MikiKru <michal_kruczkowski@o2.pl>
2019-11-29 12:04:48 +0100

    sklep v1

--- exerciseD3/P50/P50.py
+++ exerciseD3/P50/P50.py
@@ -4,6 +4,7 @@ products = {"1" : ["A", 3.5, 10],
             "3" : ["C", 9.99, 1]}
 # koszyk zawiera listy zamówionych produktów
 basket = []
+cumSum = 0
 # CLI użytkownika
 while(True):
     # pętla wypisująca produkty
@@ -13,4 +14,21 @@ while(True):
     choice = input("Co chcesz zamówić podaj id produktu lub Q-wyjście")
     # wyjście z programu
     if(choice.upper() == "Q"):
+        print("Wyjście")
         break
+    # spr czy istnieje taki id
+    if(choice not in products.keys()):
+        print("Wie ma takiego produktu")
+        continue
+    quantity = float(input("Wprowadź ilość zamawianego produktu"))
+    # sprawdzenie dostępności produktu
+    if(quantity > products[choice][2] and quantity > 0):
+        print("Dostępnych jest tylko: " + str(products[choice][2]) + "szt.")
+        continue
+    # aktualizacja magazynu i koszyka
+    products[choice][2] -= quantity # products[choice][2] = products[choice][2] - quantity
+    basket.append([products[choice][0],products[choice][1], quantity])
+    # suma skumulowana zamówień w koszyku
+    cumSum += quantity * products[choice][1]
+    print("Twój koszyk:", basket)
+    print("Suma do zapłaty:", cumSum, "zł")
